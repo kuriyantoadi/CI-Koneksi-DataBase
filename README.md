f7f87dc67a5a5109f5050a29cba116e168d887cc
kuriyantoadi <melatikuning212@gmail.com>
2019-12-01 12:34:18 +0700

    update 2

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# CI-Koneksi-DataBase
-Codeigniter Koneksi Database
+# CodeIgniter-Koneksi
+Menghubungkan CI dengan Database
+
+**1. Extrak Framework CI**
+
+**2. Import database ci-koneksi.sql**
+
+**3. application/config/autoload.php**
+  <br>Awal Baris 92 :
+  <br>`$autoload['helper'] = array('');`
+  <br>Edit :
+  <br>`autoload['helper'] = array('url');`
+
+  <br>Awal Baris 61 :
+  <br>`  $autoload['libraries'] = array('');`
+  <br>Edit :
+  <br>`  $autoload['libraries'] = array('database');`
+
+
+**4. application/config/database.php ubah baris 78-81**
+  <br>`'hostname' => 'localhost',`
+  <br>`'username' => 'root',`
+  <br>`'password' => '',`
+  <br>`'database' => 'ci-koneksi',`
+
+  **5. Tambahkan beberapa file berikut**
+  - application/controllers/Belajar.php
+  - application/view/v_user.php
+  - application/models/M_data.php 
---
+++ ci-koneksi.sql
@@ -0,0 +1,64 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.14
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Dec 01, 2019 at 06:23 
+-- Server version: 5.6.26
+-- PHP Version: 5.6.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ci-koneksi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `alamat` text NOT NULL,
+  `pekerjaan` varchar(50) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `nama`, `alamat`, `pekerjaan`) VALUES
+(1, 'um', 'serang', 'nulis');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
